7432d13cd07587152ed4dda0d402df9f39c94aed
AyushThakur05-dev <ayushthakur1680@gmail.com>
2025-07-04 12:56:40 +0530

    Time: 18 ms (62.82%) | Memory: 43.9 MB (78.83%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,23 +1,31 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] arr, int target) {
-         List<List<Integer>> result = new ArrayList<>();
-       HashSet<Long> hs = new HashSet<>();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                for (int k = j + 1; k < arr.length; k++) {
-                    long sum = arr[i] + arr[j] ;
-                    sum+=arr[k];
-                    long rem = target - sum;
-                    if (hs.contains(rem)) {
-                        List<Integer> al = Arrays.asList(arr[i], arr[j], arr[k], (int)rem);
-                        al.sort(null);
-                        if (!result.contains(al)) {
-                            result.add(al);
-                        }
-                    } hs.add((long)arr[k]);
-
+         Arrays.sort(arr);
+        List<List<Integer>> result = new ArrayList<>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                if (j > i+1 && arr[j] == arr[j - 1]) continue;
+                int k = j + 1, l = n - 1;
+                while (k < l) {
+                    long sum = arr[i];
+                    sum += arr[j];
+                    sum += arr[k];
+                    sum += arr[l];
+                    if (sum == target) {
+                        List<Integer> al = Arrays.asList(arr[i], arr[j], arr[k], arr[l]);
+                        result.add(al);
+                        k++;
+                        l--;
+                        while (k < l && arr[k] == arr[k - 1])
+                            k++;
+                        while (k < l && arr[l] == arr[l + 1])
+                            l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else l--;
                 }
-                hs.clear();
             }
         }
         return result;
