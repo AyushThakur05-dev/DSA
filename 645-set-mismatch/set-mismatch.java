87b4380665d6f2567580bf7acb2481274465fd76
AyushThakur05-dev <ayushthakur1680@gmail.com>
2025-07-07 11:46:18 +0530

    Time: 2 ms (95.31%) | Memory: 45.8 MB (44.09%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,16 +1,41 @@
 class Solution {
     public int[] findErrorNums(int[] arr) {
-         int repeating=-1 , missing=-1;
-        int[] hasharray=new int[arr.length+1];
-        for (int i = 0;i<arr.length;i++){
-            hasharray[arr[i]]++;
+        int n = arr.length;
+        int repeat = -1;
+        int missing = -1;
+        int xorarr = 0;
+        int xorton = 0;
+        for (int i = 1; i <= n; i++) {
+            xorton = xorton ^ i;
         }
-        for (int i =1;i<hasharray.length;i++){
-            if (hasharray[i]==0)missing=i;
-            if (hasharray[i]==2)repeating=i;
+        for (int i = 0; i < n; i++) {
+            xorton = xorton ^ arr[i];
         }
-        int[] result={repeating,missing};
-        return result;
+        int xor = xorarr ^ xorton;
+        int number = (xor & ~(xor - 1));
+        //creating two groups
+        int one = 0;
+        int zero = 0;
+        //comapring bits of array elements
+        for (int i = 0; i < n; i++) {
+            if ((arr[i] & number) != 0) {
+                one = one ^ arr[i];
+            } else
+                zero = zero ^ arr[i];
+        }
+        // now 1 to n
+        for (int i = 1; i <= n; i++) {
+            if ((i & number) != 0) one = one ^ i;
+            else zero = zero ^ i;
+        }
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == one) count++;
+        }
+        if (count == 2)
+           return new int[]{one,zero};
+        return new int[]{zero,one};
     }
     
 }
