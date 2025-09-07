5c7c91e52bba6eccee4d9568655be0837f2f72ea
Ayush Thakur <ayushthakur1680@gmail.com>
2025-09-07 22:22:00 +0530

    Time: 0 ms (100.00%) | Memory: 83.8 MB (79.42%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,19 +1,32 @@
 class Solution {
     public int[] findPeakGrid(int[][] arr) {
         int n=arr.length , m=arr[0].length;
+       int low=0 , high = m-1 , midcolumn=-1;
+        int left=-1,right=-1,up=-1,down=-1,index=-1;
         int[] result=new int[2];
-        int left=-1,right=-1,up=-1,down=-1;
-        for (int i =0 ; i< n ; i++){
-            for (int j =0 ; j<m ; j++){
-                up = (i>0) ? arr[i-1][j] : Integer.MIN_VALUE;
-                down = (i<(n-1)) ? arr[i+1][j] : Integer.MIN_VALUE;
-                left = (j>0) ? arr[i][j-1] : Integer.MIN_VALUE;
-                right = (j<(m-1)) ? arr[i][j+1] : Integer.MIN_VALUE;
-                if (arr[i][j]>up && arr[i][j]>down && arr[i][j]>left && arr[i][j]>right){
-                    result[0]=i;
-                    result[1]=j;
+        while (low<=high){
+            midcolumn=(low+high)/2;
+            int  max=Integer.MIN_VALUE;
+            for (int i =0 ;i<n ; i++){
+                if (arr[i][midcolumn]>=max){
+                    max=arr[i][midcolumn];
+                    index=i;
                 }
             }
+            up=(index>0) ? arr[index-1][midcolumn] : Integer.MIN_VALUE;
+            down=(index<n-1) ? arr[index+1][midcolumn] : Integer.MIN_VALUE;
+            left=(midcolumn>0) ? arr[index][midcolumn-1] : Integer.MIN_VALUE;
+            right=(midcolumn<m-1) ? arr[index][midcolumn+1] : Integer.MIN_VALUE;
+            if (arr[index][midcolumn]>left && arr[index][midcolumn]>right){
+                result[0]=index;
+                result[1]=midcolumn;
+                break;
+            } else if (arr[index][midcolumn]<left) {
+                high=midcolumn-1;
+            } else if (arr[index][midcolumn]<right) {
+                low=midcolumn+1;
+            }
+
         }
         return result;
     }
